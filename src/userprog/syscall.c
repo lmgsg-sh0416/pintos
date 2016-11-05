efa11295b4935ce13e6b951622b6f9d653ba4ee8
Lee Seonghyeon <tjdgus100495@gmail.com>
2016-11-05 12:34:07 +0900

    Divide into argument validation part and function call part

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -48,17 +48,24 @@ syscall_exit (int status)
   thread_exit();
 }
 
-static void
+static tid_t
 syscall_exec (const char *cmd_line)
 {
-  process_execute (cmd_line);
+  return process_execute (cmd_line);
 }
 
 static bool
 syscall_create (const char *name, int32_t size)
 {
+  struct thread *cur = thread_current ();
   bool result;
   
+  if (!validate_user_memory (name))
+  {
+    cur->exit_status = -1;
+    thread_exit();
+  }
+
   result = filesys_create (name, size);
 
   return result;
@@ -67,8 +74,15 @@ syscall_create (const char *name, int32_t size)
 static bool
 syscall_remove (const char *name)
 {
+  struct thread *cur = thread_current ();
   bool result;
   
+  if (!validate_user_memory (name))
+  {
+    cur->exit_status = -1;
+    thread_exit();
+  }
+
   result = filesys_remove (name);
 
   /* what if the file is opened? */
@@ -79,9 +93,23 @@ syscall_remove (const char *name)
 static int
 syscall_open (const char *name)
 {
-  struct file_desc *fd = malloc (sizeof (struct file_desc));
+  struct thread *cur = thread_current ();
+  struct file_desc *fd;
 
+  if (!validate_user_memory (name))
+  {
+    cur->exit_status = -1;
+    thread_exit ();
+  }
+
+  fd = malloc (sizeof (struct file_desc));
   fd->file = filesys_open (name);
+
+  if (fd->file == NULL)
+  {
+    free (fd);
+    return -1;
+  }
 
   if (list_empty (&fd_list))
     fd->num = 3;
@@ -129,8 +157,15 @@ syscall_close (int fd)
 static int
 syscall_read (int fd, char *buffer, unsigned size)
 {
+  struct thread *cur = thread_current ();
   int read = -1;
   
+  if (!validate_user_memory (buffer))
+  {
+    cur->exit_status = -1;
+    thread_exit();
+  }
+
   if (fd == 0)
     for (read = 0; read < size; read++)
       *(buffer + read) = input_getc ();
@@ -156,7 +191,14 @@ syscall_read (int fd, char *buffer, unsigned size)
 static int
 syscall_write (int fd, const char *buffer, unsigned size)
 {
+  struct thread *cur = thread_current ();
   int written = -1;
+
+  if (!validate_user_memory (buffer))
+  {
+    cur->exit_status = -1;
+    thread_exit();
+  }
 
   if (fd == 1)
     {
@@ -202,85 +244,72 @@ syscall_handler (struct intr_frame *f)
 
   syscall_num = *((int *)f->esp);
   
+  // Validate argument address
+  // CAUTION: Need to additional validate if value is pointer
+  switch (syscall_num) {
+    // Three argument
+    case SYS_READ:
+    case SYS_WRITE:
+      if (!validate_user_memory (f->esp+12))
+      {
+        cur->exit_status = -1;
+        thread_exit();
+      }
+    // Two argument
+    case SYS_CREATE:
+    case SYS_SEEK:
+      if (!validate_user_memory (f->esp+8))
+      {
+        cur->exit_status = -1;
+        thread_exit();
+      }
+    // One argument
+    case SYS_EXIT:
+    case SYS_EXEC:
+    case SYS_WAIT:
+    case SYS_REMOVE:
+    case SYS_OPEN:
+    case SYS_FILESIZE:
+    case SYS_TELL:
+    case SYS_CLOSE:
+      if (!validate_user_memory (f->esp+4))
+      {
+        cur->exit_status = -1;
+        thread_exit();
+      }
+    default:
+      break;
+  }
+
   switch (syscall_num) {
     case SYS_HALT:
       shutdown_power_off ();
       break;
     case SYS_EXIT:
-      if (!validate_user_memory (f->esp+4))
-      {
-        cur->exit_status = -1;
-        thread_exit();
-      }
       syscall_exit (*((int *)(f->esp+4)));
       break;
     case SYS_EXEC:
-      if (!validate_user_memory (f->esp+4))
-      {
-        cur->exit_status = -1;
-        thread_exit();
-      }
-      syscall_exec (*((char **)(f->esp+4)));
+      f->eax = syscall_exec (*((char **)(f->esp+4)));
       break;
     case SYS_WAIT:
-      if (!validate_user_memory (f->esp+4))
-      {
-        cur->exit_status = -1;
-        thread_exit();
-      }
       f->eax = process_wait (*((tid_t *)(f->esp+4)));
       break;
     case SYS_CREATE:
-      if (!validate_user_memory (f->esp + 4) ||
-          !validate_user_memory (f->esp + 8))
-      {
-        cur->exit_status = -1;
-        thread_exit ();
-      }
-
       f->eax = syscall_create (*((char **)(f->esp + 4)), *((int32_t *)(f->esp + 8)));
       break;
     case SYS_REMOVE:
-      if (!validate_user_memory (f->esp + 4))
-        {
-          cur->exit_status = -1;
-          thread_exit ();
-        }
-
       f->eax = syscall_remove (*(char **)(f->esp + 4));
       break;
     case SYS_OPEN:
-      if (!validate_user_memory (f->esp + 4))
-      {
-        cur->exit_status = -1;
-        thread_exit ();
-      }
-
       f->eax = syscall_open (*(char **)(f->esp + 4));
       break;
     case SYS_FILESIZE:
       printf ("FILESIZE!\n");
       break;
     case SYS_READ:
-      if (!validate_user_memory (f->esp + 4) ||
-          !validate_user_memory (f->esp + 8) ||
-          !validate_user_memory (f->esp + 12))
-      {
-        cur->exit_status = -1;
-        thread_exit();
-      }
-
       f->eax = syscall_read (*((int *)(f->esp + 4)), *((char **)(f->esp + 8)), *((int *)(f->esp + 12)));
       break;
     case SYS_WRITE:
-      if (!validate_user_memory (f->esp + 4) ||
-          !validate_user_memory (f->esp + 8) ||
-          !validate_user_memory (f->esp + 12))
-      {
-        cur->exit_status = -1;
-        thread_exit();
-      }
-
       f->eax = syscall_write (*((int *)(f->esp + 4)), *((char **)(f->esp + 8)), *((int *)(f->esp + 12)));
       break;
     case SYS_SEEK:
@@ -290,12 +319,6 @@ syscall_handler (struct intr_frame *f)
       printf ("TELL!\n");
       break;
     case SYS_CLOSE:
-      if (!validate_user_memory (f->esp + 4))
-        {
-          cur->exit_status;
-          thread_exit ();
-        }
-
       syscall_close (*((int *)(f->esp + 4)));
       break;
     default:
