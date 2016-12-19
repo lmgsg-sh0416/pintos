d596d83b7c35e79e756f018dbfcdffb52fa6b356
Lee Seonghyeon <sh0416@postech.ac.kr>
2016-12-18 20:23:42 -0800

    Improve cache to support asynchronous and consistent IO

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -15,9 +15,11 @@ static size_t num_cache;
 static bool
 search_buffer_cache (block_sector_t sector, struct cache **entry)
 {
+  lock_acquire (&cache_lock);
   if (num_cache == 0)
     {
       *entry = NULL;
+      lock_release (&cache_lock);
       return false;
     }
   else
@@ -29,11 +31,14 @@ search_buffer_cache (block_sector_t sector, struct cache **entry)
           if (temp->sector == sector)
             {
               *entry = temp;
+              lock_acquire (&temp->cache_lock);
+              lock_release (&cache_lock);
               return true;
             }
         }
     }
   *entry = NULL;
+  lock_release (&cache_lock);
   return false;
 }
 
@@ -44,9 +49,18 @@ fetch_buffer_cache (block_sector_t sector)
 
   if (!search_buffer_cache (sector, &entry))
     {
+      lock_acquire (&cache_lock);
       if (num_cache == CACHE_SIZE_LIMIT)
         {
-          struct cache *evicted = list_entry (list_front (&buffer_cache), struct cache, elem);
+          struct list_elem *e;
+          struct cache *evicted;
+          for (e = list_begin (&buffer_cache); e != list_end (&buffer_cache);
+               e = list_next (e))
+            {
+              evicted = list_entry (e, struct cache, elem);
+              if (lock_try_acquire (&evicted->cache_lock))
+                break;
+            }
           if (evicted->dirty)
             {
               block_write (fs_device, evicted->sector, evicted->data);
@@ -58,7 +72,10 @@ fetch_buffer_cache (block_sector_t sector)
           num_cache--;
         }
       else
-        entry = &cache_pool[num_cache];
+        {
+          entry = &cache_pool[num_cache];
+          lock_acquire (&entry->cache_lock);
+        }
 
       entry->sector = sector;
       entry->dirty = false;
@@ -66,6 +83,7 @@ fetch_buffer_cache (block_sector_t sector)
       block_read (fs_device, entry->sector, entry->data);
       list_push_back (&buffer_cache, &entry->elem);
       num_cache++;
+      lock_release (&cache_lock);
     }
 
   return entry;
@@ -102,7 +120,10 @@ init_buffer_cache ()
   cache_pool = palloc_get_multiple (PAL_USER | PAL_ZERO, num);
 
   for (i = 0; i < CACHE_SIZE_LIMIT; i++)
-    cache_pool[i].dirty = false;
+    {
+      cache_pool[i].dirty = false;
+      lock_init (&(cache_pool[i].cache_lock));
+    }
 
   list_init (&buffer_cache);
   lock_init (&cache_lock);
@@ -117,8 +138,6 @@ cache_read (block_sector_t sector, void *buffer)
   struct cache *entry;
   block_sector_t *aux;
 
-  lock_acquire (&cache_lock);
-
   entry = fetch_buffer_cache (sector);
   memcpy (buffer, entry->data, BLOCK_SECTOR_SIZE);
 
@@ -126,8 +145,7 @@ cache_read (block_sector_t sector, void *buffer)
   list_push_back (&buffer_cache, &entry->elem);
 
   ASSERT (list_size (&buffer_cache) == num_cache);
-
-  lock_release (&cache_lock);
+  lock_release (&entry->cache_lock);
 
 //  aux = malloc (sizeof *aux);
 //  if (aux != NULL)
@@ -142,8 +160,6 @@ cache_write (block_sector_t sector, const void *buffer)
 {
   struct cache *entry;
 
-  lock_acquire (&cache_lock);
-
   entry = fetch_buffer_cache (sector);
   memcpy (entry->data, buffer, BLOCK_SECTOR_SIZE);
   entry->dirty = true;
@@ -151,7 +167,7 @@ cache_write (block_sector_t sector, const void *buffer)
   list_remove (&entry->elem);
   list_push_back (&buffer_cache, &entry->elem);
   ASSERT (list_size (&buffer_cache) == num_cache);
-  lock_release (&cache_lock);
+  lock_release (&entry->cache_lock);
 }
 
 void
